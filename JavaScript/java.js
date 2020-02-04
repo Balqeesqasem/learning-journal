c6035fe5def31eeae007cf8c41ec364bf92e0081
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-02-04 20:42:01 +0200

    add html

---
+++ JavaScript/java.js
@@ -0,0 +1,10 @@
+var today = new Date(); 
+var hourNow = today.getHours();
+ var greeting; 
+ var hourNow = prompt ("Whats your budjet")
+ if (hourNow > 18) { greeting = 'Goodevening,Class!'; } 
+ else if(hourNow > 12){ greeting = 'Goodafternoon,Class!';
+ } else if(hourNow >= 0){ greeting = 'Goodmorning!'; }
+ else { greeting = 'Somethingwentwrong!';
+ } 
+ document.write('<h3>' + greeting + '</h3>');
