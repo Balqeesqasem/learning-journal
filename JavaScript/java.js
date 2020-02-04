872afb7b11bf27021f2185c0ff45294bf92b6ba7
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-02-04 23:10:32 +0200

    add js

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,10 +1,10 @@
 var today = new Date(); 
-var pay = today.getHours();
+var pay = today.getpay();
  var greeting; 
  var pay = prompt ("How much to pay")
 
  if (pay < 50) {greeting = 'There are many balloons that you can buy for less than $ 50. Enjoy our shopping' } 
- else if(hourNow > 50){ greeting = 'This is a high budget , seems that what you are preparing for will be a wonderful thing with our balloons!';
+ else if(pay > 50){ greeting = 'This is a high budget , seems that what you are preparing for will be a wonderful thing with our balloons!';
  } 
  else { greeting = 'Somethingwentwrong!';
  } 
