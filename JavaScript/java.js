0b5c1886386238aea8c9c590725c6fde3f43bc4c
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-02-05 12:06:51 +0200

    add function

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,9 +1,23 @@
 var pay = Number ;
 var greeting; 
  var pay = prompt ("How much do you want to spend")
- if (pay < 50) { greeting = 'You can find many ideas for this amount. Enjoy buying our balloons!'; } 
- else if(pay > 50){ greeting = 'That is a high budget , seems that what you are preparing for will be a wonderful thing with our balloons!';
+ 
+function creatGreeting(pay) {
+
+    if (pay < 50) 
+    {
+         greeting = 'You can find many ideas for this amount. Enjoy buying our balloons!';
+        } 
+    else if(pay > 50)
+    { greeting = 'That is a high budget , seems that what you are preparing for will be a wonderful thing with our balloons!';
+   
+    }
+    else
+    { greeting = 'Something went wrong!';
+   
+    } 
+    return greeting;
+
  }
- else { greeting = 'Somethingwentwrong!';
- } 
- document.write('<h3>' + greeting + '</h3>');
+
+ //.write('<h3>' + creatGreeting(pay) + '</h3>'); call the function in html nit here undr javascript called.
